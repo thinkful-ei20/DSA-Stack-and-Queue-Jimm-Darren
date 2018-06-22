24df3745103cc0e6c9363913375b5890a83d3770
Darren <ninenumbproductions@gmail.com>
2018-06-22 08:28:11 -0700

    starTreck

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
 class _Node {
-  constructor(value) {
-      this.value=value,
-      this.next=null,
-      this.prev=null;
+  constructor(data,next) {
+      this.data=data,
+      this.next=next
   }
 }
+
+class Stack{
+  constructor(){
+    this.top = null;
+  }
+
+  push(data){
+    if(this.top === null){
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+    const node = new _Node(data, this.top);
+    this.top = node;
+    // return this.top;
+  }
+
+  pop(){
+    // if(this.top === null){
+    //   return null;
+    // }
+    let node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
+}
+
+function main(){
+  let starTrek = new Stack();
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+
+  console.log(
+    JSON.stringify(
+      starTrek
+    ,null, 2)
+  );
+}
+main();
+
+
+
+
+
+
+
+
+
+
 
 class Queue {
   constructor() {
       this.first = null;
       this.last = null;
   }
+}
 
-  
