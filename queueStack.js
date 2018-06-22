bcc56eb89fba9f7f04271590229bcca62b621985
unknown <ajameslusk@gmail.com>
2018-06-22 17:33:00 -0400

    ophidian bank

--- queueStack.js
+++ queueStack.js
@@ -32,11 +32,9 @@ class Queue{
     this.entryQueue = new Stack(),
     this.exitQueue = new Stack();
   }
-
   enqueue(data){
     this.entryQueue.push(data);
   }
-
   dequeue(){
     if(this.exitQueue.top === null){
       while(this.entryQueue.top !== null){
@@ -45,27 +43,78 @@ class Queue{
     }
     this.exitQueue.pop();
   }
-
 }
+
+class SnakeQueue{
+  constructor(){
+    this.entryQueue = new Stack(),
+    this.exitQueue = new Stack();
+  }
+  enqueue(data){
+    console.log(`${data} entered`);
+    this.entryQueue.push(data);
+  }
+  dequeue(){
+    if(this.exitQueue.top === null){
+      while(this.entryQueue.top !== null){
+        this.exitQueue.push(this.entryQueue.pop());
+      }      
+    }
+    if(Math.random() < 0.25){
+      let patron = this.exitQueue.pop();
+      console.log(`${patron} rejected`);
+      this.enqueue(patron);
+    } else {
+      let patron = this.exitQueue.pop();
+      console.log(`${patron} accepted`);
+    }
+  }
+}
+
+
 function main(){
-  let example = new Queue();
-  example.enqueue('a');
-  example.enqueue('b');
-  example.enqueue('c');
-  example.dequeue();
-  example.enqueue('d');
-  example.enqueue('e');
-  example.dequeue();
+  // let example = new Queue();
+  // example.enqueue('a');
+  // example.enqueue('b');
+  // example.enqueue('c');
+  // example.dequeue();
+  // example.enqueue('d');
+  // example.enqueue('e');
+  // example.dequeue();
+  // console.log(JSON.stringify(example,null, 2));
 
+  let bank = new SnakeQueue();
+  // for(let i = 0; i < 25; i++){
+  //   snake.enqueue(`${i}`);
+  // }
+  // for(let j = 0; j < 25; j++){
+  //   setInterval(()=>{
+  //     snake.dequeue();
+  //   }, 2000);
+  // }
 
+  bank.enqueue('a');
+  bank.enqueue('b');
+  bank.enqueue('c');
+  bank.enqueue('d');
+  bank.enqueue('e');
+  bank.enqueue('f');
+  bank.enqueue('g');
+  bank.enqueue('h');
+  bank.enqueue('i');
+  bank.enqueue('j');
+  bank.enqueue('k');
 
-
-
-  console.log(
-    JSON.stringify(
-      example
-    ,null, 2)
-    );
   
+  bank.dequeue();
+  bank.dequeue();
+  bank.dequeue();
+  bank.dequeue();
+  bank.dequeue();
+  bank.dequeue();
+  bank.dequeue();
+  bank.dequeue();
+
+  console.log(JSON.stringify(bank,null, 2));
 }
 main();
