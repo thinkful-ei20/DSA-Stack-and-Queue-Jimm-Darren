c79624b5a894b52777e5744b28367df3cf8fd35d
Darren <ninenumbproductions@gmail.com>
2018-06-22 07:57:13 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+class _Node {
+  constructor(value) {
+      this.value=value,
+      this.next=null,
+      this.prev=null;
+  }
+}
+
+class Queue {
+  constructor() {
+      this.first = null;
+      this.last = null;
+  }
+
+  
